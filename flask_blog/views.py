679f8a839c7f5fe516a3689ce4f8e6045f54c082
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-22 12:44:29 +0900

    add flash

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -13,15 +13,17 @@ def login():
 	error = None
 	if request.method == 'POST':
 		if request.form['username'] != app.config['USERNAME']:
-			print('ユーザー名が異なります')
+			flash('ユーザー名が異なります')
 		elif request.form['password'] != app.config['PASSWORD']:
-			print('パスワードが異なります')
+			flash('パスワードが異なります')
 		else :
 			session['logged_in'] = True
+			flash('ログインしました')
 			return redirect('/')
 	return render_template('login.html')
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
+	flash('ログアウトしました')
 	return redirect('/')
