3129e95d78c40abba1957870cf80dfefcd5ed1f5
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-22 13:22:14 +0900

    Use url_for

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -5,7 +5,7 @@ from flask_blog import app
 def show_entries():
 	if not session.get('logged_in'):
 		# ログインしてない時の処理
-		return redirect('/login')
+		return redirect(url_for('login'))
 	return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET','POST'])
@@ -19,11 +19,11 @@ def login():
 		else :
 			session['logged_in'] = True
 			flash('ログインしました')
-			return redirect('/')
+			return redirect(url_for('show_entries'))
 	return render_template('login.html')
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
 	flash('ログアウトしました')
-	return redirect('/')
+	return redirect(url_for('show_entries'))
