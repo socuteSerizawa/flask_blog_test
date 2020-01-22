9f61865d6c32180ca6e7238ebe71f459ceb96232
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-22 20:34:05 +0900

    apply bluepoint

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -8,4 +8,8 @@ app.config.from_pyfile('development.cfg')
 
 db = SQLAlchemy(app)
 
-from flask_blog.views import views, entries	
+from flask_blog.views.entries import entry
+
+app.register_blueprint(entry, url_prefix = '/users')
+
+from flask_blog.views import views
