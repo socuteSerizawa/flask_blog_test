ce60ab477c9d38398f8ad441cea6407cf62ed40b
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-22 18:14:36 +0900

    Finish blog READ

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -31,3 +31,11 @@ def new_entry():
 		# ログインしてない時の処理
 		return redirect(url_for('login'))
 	return render_template('entries/new.html')
+
+@app.route('/entries/<int:id>', methods = ['GET'])
+def show_entry(id):
+	if not session.get('logged_in'):
+		# ログインしてない時の処理
+		return redirect(url_for('login'))
+	entry = Entry.query.get(id)
+	return render_template('entries/show.html', entry=entry)
